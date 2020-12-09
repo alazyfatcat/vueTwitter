08085cc46e31719d5758547615a50d56b717e883
alazyfatcat <hungdq1996hn@gmail.com>
2020-12-08 22:10:50 -0500

    initial commit

---
+++ src/components/TwitterShortProfile.vue
@@ -0,0 +1,36 @@
+<script> 
+    export default {
+        name: 'twitter-short-profile',
+        props: {
+            superhero: Object
+        },
+        methods: {
+            isFollowing(hero){
+                this.$set(hero, "isFollowing", !hero.isFollowing)
+                if(hero.isFollowing === true) {
+                    console.log(`Following ${hero.name}`)
+                }else {
+                    console.log(`Unfollowing ${hero.name}`)
+                }
+            }
+        }
+    }
+</script>
+<template> 
+    <article class="dt w-100 bb b--black-05 pb2 mt2" href="#0" >
+      <div class="dtc w2 w3-ns v-mid">
+        <img :src="superhero.pic"/>
+      </div>
+      <div class="dtc v-mid pl3">
+        <h1 class="f6 f5-ns fw6 lh-title black mv0"> {{ superhero.name }}</h1>
+        <h2 class="f6 fw4 mt0 mb0 black-60">{{ superhero.twitter }}</h2>
+      </div>
+      <div class="dtc v-mid" @submit.prevent = "isFollowing(superhero)">
+        <form class="w-100 tr">
+          <button 
+          class="f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60" 
+          type="submit"> {{ superhero.isFollowing === true ? ' - Unfollow': '+ Follow' }}</button>
+        </form>
+      </div>
+    </article>
+</template>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import tachyons from 'tachyons' 
+
+Vue.use(tachyons)
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
+
+
+
